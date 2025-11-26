5edb799d2780988dea926b5dfb63ae217638e438
Adrian <ahermoabalde@danielcastelao.org>
2025-11-26 13:34:04 +0100

    muestra el estado actual del tablero

--- tablero.py
+++ tablero.py
@@ -2,3 +2,10 @@ class Tablero:
     def __init__(self, tamano):
         self.dimensiones = (tamano, tamano)
         self.casillas = [[0 for _ in range(tamano)] for _ in range(tamano)]
+
+    def mostrar_tablero(self):
+
+        print("=== TABLERO ===")
+        for fila in self.casillas:
+            print(" ".join(str(celda) for celda in fila))
+        print("===============")
