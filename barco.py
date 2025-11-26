53a82402cc2600dfa2e3c3365706cf14064eaad3
Adrian <ahermoabalde@danielcastelao.org>
2025-11-26 13:31:15 +0100

    Muestra el estado del barco

--- barco.py
+++ barco.py
@@ -10,3 +10,12 @@ class Barco:
 
     def esta_hundido(self):
         return self.golpes_recibidos >= self.longitud
+
+    def mostrar_estado(self):
+
+        estado = "HUNDIDO" if self.esta_hundido() else "Aún a flote"
+        print(f"Barco: {self.nombre}")
+        print(f"Longitud: {self.longitud}")
+        print(f"Golpes recibidos: {self.golpes_recibidos}")
+        print(f"Estado: {estado}")
+        print("-------------")
